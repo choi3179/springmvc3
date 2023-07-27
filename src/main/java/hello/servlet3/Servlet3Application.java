39805046fe078d9a6b9b7ce5f8fb6a0481a28d79
choi <jacobis@naver.com>
2023-07-28 01:53:48 +0900

    [ADD] 07.28 스프링 MVC 학습 프로젝트 생성

---
+++ src/main/java/hello/servlet3/Servlet3Application.java
@@ -0,0 +1,13 @@
+package hello.servlet3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Servlet3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Servlet3Application.class, args);
+	}
+
+}
